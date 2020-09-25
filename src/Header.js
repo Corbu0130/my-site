6a9823099cf7ba493f25f592dab2f50b85686a70
corbu0130 <johncorbeta1997@gmail.com>
2020-09-26 00:05:41 +0800

    Initialize my site first commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <h1>Welcome to {"< Corbomode />"}</h1>
+            <nav>
+                <a href="#">Profile</a>
+                <a href="#">Projects</a>
+                <a href="#">Reference</a>
+                <a href="#">About</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
